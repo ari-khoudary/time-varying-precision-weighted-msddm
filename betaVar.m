661890b833e766c67bde38fec792a2ca57503f0f
Maria Khoudary <makhouda@uci.edu>
2022-06-29 00:27:41 -0700

    fix incorrect computation of variance of beta distribution

--- betaVar.m
+++ betaVar.m
@@ -2,6 +2,6 @@ function variance = betaVar(alpha, beta)
 
 % computes the variance of a beta distribution
 
-variance = (alpha*beta) / ((alpha+beta)^2) * (alpha + beta + 1);
+variance = (alpha*beta) / (((alpha+beta)^2) * (alpha + beta + 1));
 
 end
