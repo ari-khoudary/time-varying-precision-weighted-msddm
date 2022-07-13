f52924bf9dc00a4fdfb209bb80d9af783dc73d65
Maria Khoudary <makhouda@uci.edu>
2022-07-13 13:33:23 -0700

    take wrapper script off github

--- fullfactorialWrapper.m
+++ fullfactorialWrapper.m
@@ -5,11 +5,11 @@ antCohs = [0.5, 0.65, 0.8];
 coherences = [0.5, 0.65, 0.8];
 congruence = [0, 1];
 
-for i=1:length(cues)
+for cue=1:length(cues)
     for j = 1:length(antCohs)
         for k = 1:length(coherences)
             for l = 1:length(congruence)
-                doSampling(cues(i), antCohs(j), coherences(k), congruence(l));
+                doSampling(cues(cue), antCohs(j), coherences(k), congruence(l));
             end
         end
     end
