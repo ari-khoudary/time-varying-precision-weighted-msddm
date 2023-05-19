e49556ccc1d4a409d95ed228786b0b90d4e9018b
Maria Khoudary <makhouda@uci.edu>
2023-05-18 19:38:58 -0700

    wrapper script (in progress)

---
+++ doSampling_wrapper.m
@@ -0,0 +1,60 @@
+%% specify simulation settings
+
+nTrial = 10; % per cue
+cue = [0.5];
+coherence = [0.52];
+threshold = [3];
+memoryThinning = 12;
+visionThinning = 1;
+vizPresentationRate = 1/60;
+
+% durations in seconds
+expLambda = 0.15;
+maxNoiseDuration = 2;
+minNoiseDuration = 1;
+minSignalDuration = 0.375;
+secondSignalMin = 0; % value to be added to signalMin to create a second signal period of additional length
+
+% half neutral trials (boolean)
+halfNeutralTrials = 1;
+
+% visual evidence noise
+flickerNoisePadding = 1; %logical; do you want to pad each signal frame with a noise frame?
+flickerNoiseValue = 'zero'; %string; how do you want to model noise frames? options are zeros, zero-centered gaussian, more to come
+
+% do you want to save frame-by-frame information for each trial?
+saveEvidence = 1;
+saveAccumulators = 1;
+saveCounters = 1;
+savePrecisions = 1;
+saveDrifts = 1;
+
+%% create config file
+config.nTrial = nTrial;
+config.cue = cue;
+config.coherence = coherence;
+config.threshold = threshold;
+config.memoryThinning = memoryThinning;
+config.visionThinning = visionThinning;
+config.vizPresentationRate = vizPresentationRate;
+
+% durations in seconds
+config.trialDuration = trialDuration;
+config.maxNoiseDuration = maxNoiseDuration;
+config.minNoiseDuration = minNoiseDuration;
+config.minSignalDuration = minSignalDuration;
+config.expLambda = expLambda;
+
+% visual evidence noise
+config.flickerNoisePadding = flickerNoisePadding;
+config.flickerNoiseValue = flickerNoiseValue;
+config.saveEvidence = saveEvidence;
+config.saveAccumulators = saveAccumulators;
+config.saveCounters = saveCounters;
+config.savePrecisions = savePrecisions;
+config.saveDrifts = saveDrifts;
+
+%% run simulation
+tic
+data = doSampling(config);
+toc
