d442db3952f781f02f31c2204db4f4bf90cdf68b
Maria Khoudary <makhouda@uci.edu>
2023-06-01 18:35:02 -0700

    add code to create long-format data structure

--- doSampling_summaryPlots.m
+++ doSampling_summaryPlots.m
@@ -9,19 +9,61 @@ for i=2:length(files)
     allData(i) = load([outDir filesep files(i).name]).data;
 end
 
-%% add fields to be populated
+%% make long structure (each trial is one row)
+longData = repmat(struct('subID', [], 'nTrial', [], 'cue', [], 'coherence', [], 'congruent', [], 'threshold', [], 'memoryThinning', [], 'nFrames', [], ...
+    'noise1Frames', [], 'noise2Frames', [], 'noise2Onset', [], 'signal1Frames', [], 'rawChoice', [], 'forcedChoice', [], 'RT', []), sum([allData.nTrial]), 1);
+trialCounter = [0 cumsum([allData.nTrial])];
 
-for i = 1:length(allData)
-    congruent = logical(allData(i).congruent);
-    allData(i).congruentRawAccuracy = mean(allData(i).choices(congruent, 1));
-    allData(i).congruentForcedAccuracy = mean(allData(i).choices(congruent, 2));
-    allData(i).incongruentRawAccuracy = mean(allData(i).choices(~congruent, 1));
-    allData(i).incongruentForcedAccuracy = mean(allData(i).choices(~congruent,2));
-    allData(i).congruentRT = mean(allData(i).RT(congruent));
-    allData(i).incongruentRT = mean(allData(i).RT(~congruent));
+for i=1:length(allData)
+    sid = repmat(num2cell(allData(i).subID), allData(i).nTrial, 1);
+    [longData(trialCounter(i)+1:trialCounter(i+1)).subID] = sid{:};
+
+    trials = repmat(num2cell(allData(i).nTrial), allData(i).nTrial, 1);
+    [longData(trialCounter(i)+1:trialCounter(i+1)).nTrial] = trials{:};
+
+    cues = repmat(num2cell(allData(i).cue), allData(i).nTrial, 1);
+    [longData(trialCounter(i)+1:trialCounter(i+1)).cue] = cues{:};
+
+    cohs = repmat(num2cell(allData(i).coherence), allData(i).nTrial, 1);
+    [longData(trialCounter(i)+1:trialCounter(i+1)).coherence] = cohs{:};
+
+    threshs = repmat(num2cell(allData(i).threshold), allData(i).nTrial, 1);
+    [longData(trialCounter(i)+1:trialCounter(i+1)).threshold] = threshs{:};
+
+    thins = repmat(num2cell(allData(i).memoryThinning), allData(i).nTrial, 1);
+    [longData(trialCounter(i)+1:trialCounter(i+1)).memoryThinning] = thins{:};
+
+    frames = repmat(num2cell(allData(i).nFrames), allData(i).nTrial, 1);
+    [longData(trialCounter(i)+1:trialCounter(i+1)).nFrames] = frames{:};
+
+    cong = num2cell(allData(i).congruent);
+    [longData(trialCounter(i)+1:trialCounter(i+1)).congruent] = cong{:};
+
+    noise1 = num2cell(allData(i).noise1Frames);
+    [longData(trialCounter(i)+1:trialCounter(i+1)).noise1Frames] = noise1{:};
+
+    noise2 = num2cell(allData(i).noise2Frames);
+    [longData(trialCounter(i)+1:trialCounter(i+1)).noise2Frames] = noise2{:};
+
+    noise2o = num2cell(allData(i).noise2Onset);
+    [longData(trialCounter(i)+1:trialCounter(i+1)).noise2Onset] = noise2o{:};
+
+    sig1 = num2cell(allData(i).signal1Frames);
+    [longData(trialCounter(i)+1:trialCounter(i+1)).signal1Frames] = sig1{:};
+
+    rawC = num2cell(allData(i).choices(:,1));
+    [longData(trialCounter(i)+1:trialCounter(i+1)).rawChoice] = rawC{:};
+
+    forcedC = num2cell(allData(i).choices(:,2));
+    [longData(trialCounter(i)+1:trialCounter(i+1)).forcedChoice] = forcedC{:};
+
+    rt = num2cell(allData(i).RT);
+    [longData(trialCounter(i)+1:trialCounter(i+1)).RT] = rt{:};
+
 end
 
-
+clear sid trials cues cohs threshs thins frames cong noise1 noise2 noise2o sig1 rawC forcedC rt
+%% add fields to be populated
 
 
 
