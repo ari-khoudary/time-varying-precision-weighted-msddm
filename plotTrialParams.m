2463b0f2b5edd936c3a2c7c07cc1d525e12e8600
Maria Khoudary <makhouda@uci.edu>
2023-11-27 17:51:57 -0800

    trial-level viz of time-varying model params

---
+++ plotTrialParams.m
@@ -0,0 +1,59 @@
+% plot precisions, drift rates, accumulators for single trials
+
+trial = 1;
+
+% make dummy mem evidence variable for unweighted accumulator plot
+memEvidence = zeros(nFrames, 1);
+memEvidence(mod(1:nFrames, memoryThinning)==1) = ...
+    memoryStream(mod(1:nFrames, memoryThinning)==1, trial);
+
+% set up figure
+time = 1:nFrames;
+alpha = 0.15;
+t = tiledlayout('flow');
+
+% estimated precision
+nexttile;
+hold on
+plot(time, memoryPrecisions(:,trial));
+plot(time, visionPrecisions(:,trial));
+xregion(0, noise1Frames(trial), FaceAlpha=alpha);
+xregion(noise2Onsets(trial), signal2Onsets(trial), FaceAlpha=alpha);
+legend({'memory precision', 'vision precision'});
+title('estimated precision');
+
+% drift rates
+nexttile;
+hold on
+plot(time, memoryDrift(:, trial));
+plot(time, visionDrift(:, trial));
+plot(time, visionDrift(:,trial) + memoryDrift(:,trial));
+xregion(0, noise1Frames(trial), FaceAlpha=alpha);
+xregion(noise2Onsets(trial), signal2Onsets(trial), FaceAlpha=alpha);
+legend({'memory drift', 'vision drift', 'DV drift (mem+viz)'});
+title('drift rates')
+
+% accumulators
+nexttile;
+hold on
+plot(time, memoryAccumulator(:,trial));
+plot(time, visionAccumulator(:, trial));
+plot(time, decisionVariable(:, trial));
+xregion(0, noise1Frames(trial), FaceAlpha=alpha);
+xregion(noise2Onsets(trial), signal2Onsets(trial), FaceAlpha=alpha);
+legend({'memory accumulator', 'vision accumulator', 'DV'});
+title('reliability-weighted accumulators')
+
+% cumsum
+nexttile;
+hold on
+plot(time, cumsum(memEvidence));
+plot(time, cumsum(flickerStream(:, trial)));
+plot(time, decisionVariable(:, trial));
+xregion(0, noise1Frames(trial), FaceAlpha=alpha);
+xregion(noise2Onsets(trial), signal2Onsets(trial), FaceAlpha=alpha);
+legend({'memory', 'vision', 'DV (reliability-weighted)'});
+title('unweighted cumulative sum')
+
+title(t, ['example trial (#' num2str(trial) '): ' num2str(cue) ' cue, ' num2str(coherence) ' coh, 1:' num2str(memoryThinning) ' mem:viz'])
+subtitle(t, ['gray bars = noise periods; flickerAdditiveNoise = ' num2str(flickerAdditiveNoise)]);
