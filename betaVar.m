c8abe06859f1cccbd89a003a83d448bdedb3f928
Maria Khoudary <makhouda@uci.edu>
2023-12-04 10:32:20 -0800

    tidy repo

--- betaVar.m
+++
@@ -1,7 +0,0 @@
-function variance = betaVar(alpha, beta)
-
-% computes the variance of a beta distribution
-variance = zeros(length(alpha), 1);
-variance(:, 1) = (alpha(:, 1).*beta(:, 1)) ./ (((alpha(:, 1)+beta(:,1)).^2) .* (alpha(:,1) + beta(:,1) + 1));
-
-end
