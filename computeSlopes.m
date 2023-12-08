707a25b8b8cc997d9844c8d5b5d1a554ae495747
Maria Khoudary <makhouda@uci.edu>
2023-12-07 18:38:07 -0800

    script to compute & store slope of DV during trial epochs

---
+++ computeSlopes.m
@@ -0,0 +1,28 @@
+% compute slope of DV from pre-generated data structure
+
+[allData(:).dvSlopes] = deal(zeros(100,4));
+
+for i = 1:height(allData)
+    % initialize variables
+    noise1 = allData(i).noise1Frames;
+    signal1 = noise1 + allData(i).signal1Frames;
+    noise2 = noise1 + signal1 + allData(i).noise2Frames;
+    signal2 = noise1 + signal1 + noise2 + allData(i).signal2Frames;
+    dv = allData(i).decisionVariable;
+
+    % compute slopes: (y2-y1) / (x2-x1)
+    % first noise period
+    allData(i).dvSlopes(:,1) = dv(noise1) ./ noise1;
+    
+    % first signal period
+    allData(i).dvSlopes(:,2) = (dv(signal1) - dv(noise1)) ./ (signal1 - (noise1));
+
+    % second noise period
+    allData(i).dvSlopes(:,3) = (dv(noise2) - dv(signal1)) ./ (noise2 - (signal1));
+
+    % second signal period
+    allData(i).dvSlopes(:,4) = (dv(signal2) - dv(noise2)) ./ (signal2 - (noise2));
+
+end
+
+
